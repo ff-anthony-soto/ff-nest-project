236e7ce4c34a5e0bf650952713336a36acdc4091
Anthony <anthony@firstfactory.com>
2020-02-07 17:08:38 -0600

    Human test issue solved

--- src/api/human/test/human.module.mock.ts
+++ src/api/human/test/human.module.mock.ts
@@ -5,8 +5,12 @@ import { HumanResolver } from '@api/human/human.resolver';
 import { HumanService } from '@api/human/human.service';
 import { Human } from '@shared/datasource/database/model/human.entity';
 import { CatHuman } from '@shared/datasource/database/model/cat-human.entity';
+import { FileService } from '@shared/modules/file/file.service';
+import { S3Service } from '@shared/datasource/aws/s3/s3.service';
+import { ConfigService } from '@shared/config/config.service';
 
 export default Test.createTestingModule({
+  imports: [],
   providers: [
     HumanResolver,
     HumanService,
@@ -18,6 +22,12 @@ export default Test.createTestingModule({
       provide: getRepositoryToken(CatHuman),
       useValue: {},
     },
+    {
+      provide: ConfigService,
+      useValue: new ConfigService('.env.development'),
+    },
+    FileService,
+    S3Service,
   ],
   exports: [HumanService],
 });
